a01c60e1950b6a01bb3ce6db9c5bcc37736490fa
aki bukz <akibukzwork@gmail.com>
2025-10-26 18:10:32 +0800

    add: coverage for jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  // Enable coverage collection (merged from jest.config.json)
+  collectCoverage: true,
+    collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',       // include all TS/TSX files
+    'src/*.{ts,tsx}',       // include all TS/TSX files
+    '!src/**/*.d.ts',          // ignore type definitions
+    '!src/**/__tests__/**',    // ignore test files
+  ],
   testMatch: ['**/tests/**/*.test.[jt]s?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
