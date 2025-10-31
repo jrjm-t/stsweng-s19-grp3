40d4ef470ffb61f88c9a717a8471be0078745b99
Allen Chavez <allen_chavez@dlsu.edu.ph>
2025-10-31 16:49:19 +0800

    preparing jest

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(marked|html2pdf\\.js))/"
+  ],
+};
