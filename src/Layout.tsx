0fb9b7d9ace9bbe4abb4de4fc6b4af9319d633d4
Dencel Angelo D. Pineda <pinedadencelangelod@gmail.com>
2025-11-02 19:01:42 +0800

    fix: add missing isOpen and setIsOpen props for Sidebar in Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./Layout.css";
 import NavBar from "./components/Sidebar";
 
 function Layout() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex">
       <nav>
-        <NavBar />
+        <NavBar isOpen={isOpen} setIsOpen={setIsOpen} />
       </nav>
       <main>
         <h2>Main Content</h2>
