ae085dd47f9317a3098ff895a7ef6effdd4a7d9a
Dencel Angelo D. Pineda <pinedadencelangelod@gmail.com>
2025-11-02 23:25:14 +0800

    chore: Update baseUrl in Cypress configuration
    align with allen localhost url

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     // IMPORTANT: modify baseUrl as per your server's address and port
-    baseUrl: "http://localhost:4173",
+    baseUrl: "http://localhost:5173",
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
