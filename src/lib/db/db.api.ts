e8adc6047256bfb04b70b47343a44767a0aac3c8
Dencel Angelo D. Pineda <pinedadencelangelod@gmail.com>
2025-11-02 19:19:18 +0800

    chore: promise typescript that parameters of type 'SetStateAction<number>' is not strings
    just to remove all typecheck errors in ci

--- src/lib/db/db.api.ts
+++ src/lib/db/db.api.ts
@@ -1350,7 +1350,7 @@ export const inventoryApi = {
   /**
    * Get total quantities added and taken within the current month.
    */
-  async getMonthlyTransactionSummary() {
+  async getMonthlyTransactionSummary(): Promise<{ itemsAdded: number; itemsTaken: number }> {
     const startDate = new Date(
       new Date().getFullYear(),
       new Date().getMonth(),
