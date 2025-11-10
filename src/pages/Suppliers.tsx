a193ba357e926479d60dad2e08234f307eb578cf
lonebono <vaughnsy15@gmail.com>
2025-11-10 13:02:38 +0800

    feat: set base code structure for Suppliers

--- src/pages/Suppliers.tsx
+++ src/pages/Suppliers.tsx
@@ -1,5 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import SupplierTable from "../components/Supplier/SupplierTable";
 import SupplierCard from "../components/Supplier/SupplierCard";
 //import { getSuppliers } from "../api/supplierApi";
 //import { Supplier } from "../types/Supplier";
+
+const columns = [
+  { key: "name", label: "Supplier" },
+  { key: "phoneNumber", label: "Phone Number" },
+  { key: "emailAddress", label: "Email Address" },
+  { key: "Remarks", label: "Remarks" },
+];
+
+const ROWS_PER_PAGE = 9;
+
+function Suppliers() {}
+
+export default Suppliers;
